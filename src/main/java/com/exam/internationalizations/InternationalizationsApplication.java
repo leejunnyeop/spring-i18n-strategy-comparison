5c8c1e47df6af4c23414d819bf3c760348bc3507
leejunnyeop <daffodil8620@naver.com>
2025-07-02 19:11:05 +0900

    초기화

---
+++ src/main/java/com/exam/internationalizations/InternationalizationsApplication.java
@@ -0,0 +1,13 @@
+package com.exam.internationalizations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalizationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalizationsApplication.class, args);
+	}
+
+}
