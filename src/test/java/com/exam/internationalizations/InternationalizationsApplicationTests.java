becc667c90f06bb9a4efa34b584d28446007ada3
leejunnyeop <daffodil8620@naver.com>
2025-07-02 19:50:00 +0900

    두번째 테스트

---
+++ src/test/java/com/exam/internationalizations/InternationalizationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.internationalizations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
